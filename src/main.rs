abad693929d064dcc4bde5f297ee9367d7df84ea
Artem Pyanykh <artem.pyanykh@gmail.com>
2021-03-27 20:22:01 +0000

    Use pending paths queue instead of recursion

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-use std::{
-    env,
-    error::Error,
-    fs, io,
-    path::{Path, PathBuf},
-};
+use std::{env, error::Error, fs, io, path::PathBuf};
 
 use clap::{crate_authors, crate_version, Clap};
 
@@ -24,29 +19,32 @@ fn main() -> Result<(), Box<dyn Error>> {
         _ => env::current_dir()?,
     };
 
-    let usage = calc_space_usage(&start_dir)?;
+    let usage = calc_space_usage(start_dir.clone())?;
     let human_usage = usage.file_size(file_size_opts::CONVENTIONAL)?;
 
     println!("{}\t{}", human_usage, start_dir.display());
     Ok(())
 }
 
-fn calc_space_usage(path: &Path) -> Result<u64, io::Error> {
-    let meta = fs::symlink_metadata(&path)?;
-    let file_type = meta.file_type();
-
+fn calc_space_usage(path: PathBuf) -> Result<u64, io::Error> {
+    let mut paths = vec![path];
     let mut size = 0;
 
-    if file_type.is_symlink() {
-        // don't follow symlinks
-    } else if file_type.is_dir() {
-        let entries = fs::read_dir(&path)?;
-        for entry in entries {
-            let entry = entry?;
-            size += calc_space_usage(&entry.path())?;
+    while let Some(path) = paths.pop() {
+        let meta = fs::symlink_metadata(&path)?;
+        let file_type = meta.file_type();
+
+        if file_type.is_symlink() {
+            // don't follow symlinks
+        } else if file_type.is_dir() {
+            let entries = fs::read_dir(&path)?;
+            for entry in entries {
+                let entry = entry?;
+                paths.push(entry.path());
+            }
+        } else if file_type.is_file() {
+            size += meta.len();
         }
-    } else if file_type.is_file() {
-        size = meta.len();
     }
 
     Ok(size)
