3dcd7c264131825e0251eee2714c74367cc474c7
Artem Pyanykh <artem.pyanykh@gmail.com>
2021-03-27 18:02:44 +0000

    Non-recursive async

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, error::Error, future::Future, io, path::PathBuf, pin::Pin};
+use std::{env, error::Error, io, path::PathBuf};
 
 use clap::{crate_authors, crate_version, Clap};
 
@@ -29,25 +29,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn calc_space_usage(path: PathBuf) -> Pin<Box<dyn Future<Output = Result<u64, io::Error>>>> {
-    Pin::from(Box::new(async move {
+async fn calc_space_usage(path: PathBuf) -> Result<u64, io::Error> {
+    let mut meta_queue = vec![path];
+    let mut size = 0;
+
+    while let Some(path) = meta_queue.pop() {
         let meta = fs::symlink_metadata(&path).await?;
         let file_type = meta.file_type();
-
-        let mut size = 0;
 
         if file_type.is_symlink() {
             // don't follow symlinks
         } else if file_type.is_dir() {
             let mut entries = fs::read_dir(&path).await?;
             while let Some(entry) = entries.next_entry().await? {
-                let entry = entry;
-                size += calc_space_usage(entry.path()).await?;
+                meta_queue.push(entry.path());
             }
         } else if file_type.is_file() {
-            size = meta.len();
+            size += meta.len();
         }
+    }
 
-        Ok(size)
-    }))
+    Ok(size)
 }
