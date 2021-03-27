c34318d4c1f4fe87336d6733003e6cdf01b64531
Artem Pyanykh <artem.pyanykh@gmail.com>
2021-03-27 16:59:12 +0000

    Add basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, error::Error, path::PathBuf};
+
+use clap::{crate_authors, crate_version, Clap};
+
+#[derive(Clap)]
+#[clap(version = crate_version!(), author = crate_authors!())]
+/// Calculate space usage of a directory tree
+struct Opts {
+    /// Directory to start from (default = current directory)
+    dir: Option<PathBuf>,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let opts = Opts::parse();
+    let start_dir = match opts.dir {
+        Some(dir) => dir,
+        _ => env::current_dir()?,
+    };
+
+    println!("Starting from {}", start_dir.display());
+    Ok(())
 }
